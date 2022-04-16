2383bcf32eeaf97fbf9f3195727e9efc66169bc4
Luiz G <luiz007@live.com>
2022-04-16 15:51:02 -0300

    implemented from python version (see https://github.com/luiz734/search-algorithms)

--- src/genetic_algorithm.rs
+++ src/genetic_algorithm.rs
@@ -1,9 +1,9 @@
 
-pub mod TSP {
-   use rand::Rng;
+pub mod tsp{
+   use rand::{Rng, seq::SliceRandom, distributions::{Distribution, Uniform}};
    use num::pow;
 
-   #[derive(Debug)]
+   #[derive(Debug, Clone)]
    pub struct Point {
       x: f32,
       y: f32
@@ -21,23 +21,27 @@ pub mod TSP {
       }
    }
    #[derive(Debug)]
-   pub struct tsp {
+   pub struct Tsp {
       n_cities: usize,
       populazion_size: usize,
       cities: Vec<Point>,
       distance_matrix: Vec<Vec<f32>>,
+      population: Vec<Individual>,
+      mutation_rate: f32,
    }
-   impl tsp {
-      pub fn new(n_cities: usize, populazion_size: usize) -> tsp {
-         tsp {
+   impl Tsp {
+      pub fn new(n_cities: usize, populazion_size: usize, mutation_rate: f32) -> Tsp {
+         Tsp {
             n_cities,
             populazion_size,
             cities: Vec::new(),
             distance_matrix: Vec::new(),
+            population: Vec::new(),
+            mutation_rate,
          }
       }
       pub fn generate_cities(&mut self) {
-         for i in 0..self.n_cities {
+         for _i in 0..self.n_cities {
             self.cities.push(Point::create_random());
          }
       }
@@ -51,19 +55,148 @@ pub mod TSP {
             }
          }         
       }
-     
+      pub fn generate_population(&mut self) {
+         for _i in 0..self.populazion_size {
+            let mut individual = Individual::new();
+            individual.generate_and_shuffle_cities(self.n_cities);
+            individual.calc_fitness(&self);
+            self.population.push(individual);
+         }
+      }
+      pub fn weighted_index(&mut self) -> usize {
+         let weights: Vec<f32> = self.population.iter().map(|idv|1./(pow(idv.fitness, 8)+1.)).collect();
+         let total: f32 = weights.iter().sum();
+         let weights: Vec<f32> = weights.iter().map(|x|x/total).collect();
+         // let total: f32 = weights.iter().sum();
+         
+         let mut rng = rand::thread_rng(); 
+         let random_value = rng.gen::<f32>(); 
+         let mut current_value = 0.;
+         for i in 0..weights.len() {
+            current_value += weights.get(i).unwrap();
+            if random_value < current_value {
+               return i
+            }
+         }
+         weights.len() - 1
+      }
+      pub fn weighted_random_parents(&mut self) -> (Individual, Individual){
+         let index_a = self.weighted_index(); 
+         let mut index_b = self.weighted_index(); 
+         while index_a == index_b {
+            index_b = self.weighted_index(); 
+         }
+         
+         // vec![self.population[index_a].clone(), self.population[index_b].clone()]
+         (self.population[index_a].clone(), self.population[index_b].clone())
+      }
+      pub fn mutate(&self, child: &mut Individual) {
+         let mut rng = rand::thread_rng(); 
+         let choices = Uniform::from(1..self.n_cities);
+         let random_index_a = choices.sample(&mut rng);
+         let random_index_b = choices.sample(&mut rng);
+         child.swap_cities(random_index_a, random_index_b);
+         child.calc_fitness(&self);
+      }
+      pub fn find_best_individual(&self) -> Individual {
+         let mut best = self.population[0].clone();
+         for i in 1..self.population.len() {
+            let current_individual = self.population[i].clone();
+            let current_fitness = current_individual.fitness;
+            if current_fitness < best.fitness {
+               best = current_individual;
+            } 
+         }
+         best
+      }
+      pub fn reproduce(&self, parent_a: &Individual, parent_b: &Individual) -> Individual {
+         let mut rng = rand::thread_rng(); 
+         let mut choices = Uniform::from(1..self.n_cities - 2);
+         let rand_start = choices.sample(&mut rng);
+         choices = Uniform::from(rand_start + 1..self.n_cities);
+         let rand_end = choices.sample(&mut rng);
+         // choices = Uniform::from(rand_start..rand_end);
+         // let index = choices.sample(&mut rng);
+         // FIX this
+         // let mut child = vec![999; self.n_cities];
+         let mut child = vec![999; rand_end - rand_start]; 
+         child.clone_from_slice(&parent_a.cities[rand_start..rand_end]);
+         for i in 0..self.n_cities {
+            if !child.contains(&parent_b.cities.get(i).unwrap()) {
+               child.push(parent_b.cities.get(i).unwrap().clone());
+            }
+         }
+         let mut child = Individual::new_from_cities(child);
+         child.calc_fitness(&self);
+         child
+      }
+      pub fn run(&mut self) {
+         let mut generations: usize = 0;
+         let mut best_individual = self.population[0].clone();
+         loop {
+            let mut next_population:Vec<Individual> = Vec::new();
+            for _i in 0..self.populazion_size {
+               let (parent_a, parent_b) = self.weighted_random_parents();
+               let mut child = self.reproduce(&parent_a, &parent_b);
+               let mut rng = rand::thread_rng(); 
+               let random_value = rng.gen::<f32>(); 
+               
+               if random_value < self.mutation_rate {
+                  self.mutate(&mut child);
+               }
+                 
+               next_population.push(child);
+            }
+            let best_in_gen = self.find_best_individual();
+            if generations % 250 == 0 {
+               println!("Gen: {}", generations);
+            }
+            if best_in_gen.fitness < best_individual.fitness {
+               best_individual = best_in_gen;
+               println!("Gen: {} {:?}", generations, best_individual);
+            }
+            generations += 1;
+            self.population = next_population;
+         }
+      }
    }
-   struct Individual {
-      cities: Vec<u8>,
+   
+   #[derive(Debug, Clone)]
+   pub struct Individual {
+      cities: Vec<usize>,
       fitness: f32,
    }
    impl Individual {
-      fn calc_fitness(&mut self) {
-         let mut sum = 0;
+      fn new() -> Individual {
+         Individual {
+            cities: Vec::new(),
+            fitness: 0.,
+         }
+      }
+      fn new_from_cities(cities: Vec<usize>) -> Individual {
+         Individual {
+            cities,
+            fitness: 0.,
+         }
+      }
+      fn generate_and_shuffle_cities(&mut self, n_cities: usize) {
+         let mut rng = rand::thread_rng(); 
+         self.cities = (0..n_cities).map(|x|x).collect();
+         self.cities.shuffle(&mut rng);
+      }
+      fn calc_fitness(&mut self, tsp: &Tsp) {
+         let mut sum = 0.;
          for i in 0..self.cities.len() {
                let city_a = self.cities.get(i%self.cities.len()).unwrap(); 
                let city_b = self.cities.get((i+1)%self.cities.len()).unwrap(); 
+               sum += tsp.distance_matrix[*city_a][*city_b];
          } 
+         self.fitness = sum;
+      }
+      fn swap_cities(&mut self, index_a: usize, index_b:usize) {
+         let tmp = self.cities[index_a];
+         self.cities[index_a] = self.cities[index_b];
+         self.cities[index_b] = tmp;
       }
    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod genetic_algorithm;
-use genetic_algorithm::TSP;
+use genetic_algorithm::tsp;
 
 fn main() {
-    let mut tsp = TSP::tsp::new(8, 10);
+    let mut tsp = tsp::Tsp::new(30, 200, 0.05);
     tsp.generate_cities();
     tsp.generate_distance_matrix();
+    tsp.generate_population();
+    tsp.run();
     println!("{:#?}", tsp);
 
 }
